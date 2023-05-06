7d63af0bf26909d9422352bae80b3eec0bb9cf75
razal <oungaounga@gmail.com>
2023-05-06 15:21:21 +0200

    first commit, set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen bg-slate-900">
+      <p className="text-center text-white text-3xl">Hello Omio Booking Test</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
